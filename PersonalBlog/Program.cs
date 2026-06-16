9de6504cd215cdd5ff603846c032e3d70386520e
Cks <qq1528359634@gmail.com>
2026-06-16 22:35:17 +0900

    add postService

--- PersonalBlog/Program.cs
+++ PersonalBlog/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using PersonalBlog.Data;
 using Scalar.AspNetCore;
+using PersonalBlog.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +10,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddRazorPages();
 builder.Services.AddOpenApi();
+builder.Services.AddScoped<PostService>();
 builder.Services.AddDbContext<BlogDbCotext>(options =>
 {
     var connectionString = builder.Configuration.GetConnectionString("PersonalBlog");
@@ -17,8 +19,8 @@ builder.Services.AddDbContext<BlogDbCotext>(options =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
 {
+if (app.Environment.IsDevelopment())
     app.MapOpenApi();
     app.MapScalarApiReference();
 }
---
+++ PersonalBlog/Services/PostService.cs
@@ -0,0 +1,58 @@
+﻿using PersonalBlog.Data;
+using PersonalBlog.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace PersonalBlog.Services
+{
+    public class PostService
+    {
+        private readonly BlogDbCotext _Context;
+
+        public PostService(BlogDbCotext Context)
+        {
+            this._Context = Context;
+        }
+
+
+        public async Task<List<Post>> GetPublishedPosts()
+        {
+            return await _Context.Posts
+                .Where(p => p.IsPublished)
+                .OrderByDescending(p => p.CreatedAt)
+                .ToListAsync();
+        }
+
+        //找寻某篇文章 
+        //あるpostを特定する
+        public async Task<Post?> GetPostBySlug(string slug)
+        {
+            return await _Context.Posts
+                .FirstOrDefaultAsync(p => p.Slug == slug && p.IsPublished);
+        }
+
+        //新增文章
+        public async Task CreatPostAsync(Post post)
+        {
+            _Context.Posts.Add(post);
+            await _Context.SaveChangesAsync();
+        }
+        //更新文章
+        public async Task UpdatePostAsync(Post post)
+        {
+            post.UpdatedAt = DateTime.UtcNow;
+            _Context.Update(post);
+            await _Context.SaveChangesAsync();
+        }
+        //删除文章
+        public async Task DeletePostAsync(int id)
+        {
+            var post =await _Context.Posts.FindAsync(id);
+            if (post != null)
+            {
+                _Context.Posts.Remove(post);
+             await _Context.SaveChangesAsync();
+            }
+        }
+
+    }
+}
