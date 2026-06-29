70b86b7c924d63dec984eaef863aad96cd10bd1f
Cks <qq1528359634@gmail.com>
2026-06-29 22:11:51 +0900

    add Admin/Index
    add razorPage and index.cs

---
+++ PersonalBlog/Pages/Admin/Logout.cshtml.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PersonalBlog.Pages.Admin
+{
+    public class LogoutModel:PageModel
+    {
+        public async Task<IActionResult> OnPostAsync()
+        {   //sign out Cookie
+            await HttpContext.SignOutAsync("BlogCookies");
+            return RedirectToPage("/Posts/Index");
+        }
+    }
+}
