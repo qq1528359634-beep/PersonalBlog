704267d717a68d7e6a5fe486f673d760429aa15b
Cks <qq1528359634@gmail.com>
2026-07-06 17:48:07 +0900

    Update Program.cs

--- PersonalBlog/Program.cs
+++ PersonalBlog/Program.cs
@@ -34,19 +34,18 @@ using (var scope = app.Services.CreateScope())
     var db = scope.ServiceProvider.GetRequiredService<BlogDbContext>();
     db.Database.Migrate();
 }
-
 // Configure the HTTP request pipeline.
 
 if (app.Environment.IsDevelopment())
     {
         app.MapOpenApi();
         app.MapScalarApiReference();
-        app.UseHttpsRedirection();
+       
     }
 
 app.UseStaticFiles();
 
-
+app.UseHttpsRedirection();
 
 app.UseAuthentication();//Who you are
 
